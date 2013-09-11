0474d4003fa2c84cdbbf6f622dc42c0a1b8097c3
Daniel <daniel@daniel.(none)>
2013-09-11 11:53:17 -0400

    Project skeleton

---
+++ mtgdrafttools/cards.py
@@ -0,0 +1,5 @@
+
+
+class Card(object):
+    def __init__(self):
+        pass
---
+++ mtgdrafttools/pools/packs.py
@@ -0,0 +1,14 @@
+# Responsible for generating packs
+
+
+
+def generate_pack(expansion_by_rarity):
+    """
+    Generates a pack of data for a given expansion.
+
+    @param expansion_by_rarity dict with keys 'common', 'uncommon', 'rare' and set
+        of cards with that rarity.  Lands will not be included and neither will 
+        the rule cards
+    @return list of cards
+    """
+    pass
